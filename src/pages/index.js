c988f1730cccce9333e2e3145ca8976d67a0483f
Heli Sivunen <heli@mooncat.tv>
2020-10-05 11:29:11 +0100

    Add background image with css.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,9 +9,6 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Synttärionnittelut maailman parhaimmalle äitille!! <span role="img" aria-label="Hearts">❤️❤️❤️</span></h1>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
   </Layout>
 )
