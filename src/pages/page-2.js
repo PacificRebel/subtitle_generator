06c1e7e2a30a7c187a618f7b4b57ac1b1b969710
Heli Sivunen <heli@mooncat.tv>
2021-07-13 15:47:41 +0100

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <br></br>
     <br></br>
-    <h1>Happy Birthday Suzi!! <span role="img" aria-label="Smiley"> 😊</span> <span role="img" aria-label="Hearts">❤️</span></h1>
+    <h1>Don't know what to do with this page yet <span role="img" aria-label="Smiley"> 😊</span> <span role="img" aria-label="Hearts">❤️</span></h1>
 
 
     <Link to="/">Back to the home page</Link>
